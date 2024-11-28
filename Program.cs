c98f90114d4449a1983ac4e5bae4452c752d1dfc
RizEoR <dor.rost@bk.ru>
2024-11-28 17:34:27 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using InspectorLib;
+
+namespace InspectorTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            FunctionInsp inspector = new FunctionInsp();
+
+            // Тестирование метода GetInspector()
+            Console.WriteLine($"Главный инспектор: {inspector.GetInspector()}");
+
+            // Тестирование метода GetCarInspection()
+            Console.WriteLine($"Название автоинспекции: {inspector.GetCarInspection()}");
+
+            // Тестирование метода SetInspector()
+            Console.WriteLine("Выбор нового главного инспектора:");
+            inspector.SetInspector();
+            Console.WriteLine($"Новый главный инспектор: {inspector.GetInspector()}");
+
+            // Тестирование метода GenerateNumber()
+            Console.WriteLine($"Сгенерированный номер: {inspector.GenerateNumber()}");
+
+            // Тестирование метода GetWorker()
+            Console.WriteLine("Список сотрудников:");
+            foreach (var worker in inspector.GetWorker())
+            // Перебираем массив сотрудников
+            {
+                Console.WriteLine(worker);
+            }
+
+            // Тестирование метода AddWorker()
+            Console.Write("Введите ФИО нового сотрудника: ");
+            string newWorker = Console.ReadLine(); // Считываем ФИО нового сотрудника
+            inspector.AddWorker(newWorker); // Добавляем нового сотрудника
+            Console.WriteLine("Список сотрудников после добавления нового:");
+            foreach (var worker in inspector.GetWorker()) 
+                // Перебираем обновленный массив
+
+            {
+                Console.WriteLine(worker);
+            }
+        }
+    }
+}
